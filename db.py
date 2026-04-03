4e1a243150e5c32e4e9cf1e9357ef7d30d07da86
manutevari <37774043+manutevari@users.noreply.github.com>
2026-04-03 21:33:45 +0530

    Delete db.py

--- db.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine, text
-import os
-
-engine = create_engine(os.getenv("DB_URL"))
-
-def setup_db():
-    with engine.connect() as conn:
-        conn.execute(text("CREATE EXTENSION IF NOT EXISTS vector;"))
